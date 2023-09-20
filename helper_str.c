96d0802a01e5b8b3988425d6b5714f0732320052
ChineduSE <122818368+ChineduSE@users.noreply.github.com>
2023-09-20 04:54:51 -0700

    Update helper

--- helper_str.c
+++ helper_str.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * str_length - returns the length of a string.
- * @string: pointer to string.
- * Return: length of string.
+ * str_length - returns the length of a string
+ * @string: pointer to string
+ * Return: length of string
  */
 int str_length(char *string)
 {
